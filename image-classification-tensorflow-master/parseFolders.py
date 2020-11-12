abe2848e654f6c78fc827b06b71e56f987ec1980
farouk877 <faroukmarhaba1@gmail.com>
2020-11-12 11:23:51 -0500

    preparing for big training

--- image-classification-tensorflow-master/parseFolders.py
+++ image-classification-tensorflow-master/parseFolders.py
@@ -5,17 +5,15 @@ import string
 # img = np.asarray((imageio.imread('./A1.jpg')))
 # print(img.shape)
 
-source = './dataset/'
-destination = './dataset/'
+source = './datasetProcessed800/'
+destination = './datasetProcessed800organized/'
 
 #create folders A-Z
 
-alphaList = list(string.ascii_uppercase)
-# alphaList = list(string.ascii_uppercase)[3:]
-# alphaList = ['nothing', 'del', 'space']
-# print(alphaList)
-# for al in alphaList:
-#     os.mkdir('./dataset/' + str(al))
+letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "space", "nothing", "del"]
+
+for letter in letters:
+    os.mkdir(destination + letter)
 
 
 
@@ -31,9 +29,40 @@ alphaList = list(string.ascii_uppercase)
 
 # # print('normal_A31.jpg'[7:])
 # print('processed_del2.jpg'[10:13])
+
+alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+special = ["space", "nothing", "del"]
+
 for filename in os.listdir(source): 
     if filename.endswith('.jpg') and not filename == '.jpg':   
-        # print(filename)
-        # if filename[10] in alphaList:
+        print(filename)
         if filename[10:15] == 'space':
-            os.rename(source + str(filename), destination + 'space' + '/' + filename[10:])
+            os.rename(source + filename, destination + filename[10:15] + '/' + filename[10:])
+        elif filename[10:17] == 'nothing':
+            os.rename(source + filename, destination + filename[10:17] + '/' + filename[10:])
+        elif filename[10:13] == 'del':
+            os.rename(source + filename, destination + filename[10:13] + '/' + filename[10:])
+        else:
+            os.rename(source + filename, destination + filename[10] + '/' + filename[10:])
+        
+        
+#         print(filename)
+#         break
+#         # print(filename)
+#         # if filename[10] in alphaList:
+#         if filename[10:15] == 'space':
+#             os.rename(source + str(filename), destination + 'space' + '/' + filename[10:])
+
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
