319b74bb3d43f18f956d47c315a1273dfca90965
farouk877 <faroukmarhaba1@gmail.com>
2020-11-19 14:46:32 -0500

    edited run.sh file

--- image-classification-tensorflow-master/run.sh
+++ image-classification-tensorflow-master/run.sh
@@ -1,8 +1,8 @@
 python train.py \
-  --bottleneck_dir=logs/bottlenecks \
+  --bottleneck_dir=logs/pipelineB/bottlenecks \
   --how_many_training_steps=2000 \
   --model_dir=inception \
-  --summaries_dir=logs/training_summaries/basic \
-  --output_graph=logs/trained_graph.pb \
-  --output_labels=logs/trained_labels.txt \
-  --image_dir=./dataset
+  --summaries_dir=logs/pipelineB/training_summaries/basic \
+  --output_graph=logs/pipelineB/trained_graph.pb \
+  --output_labels=logs/pipelineB/trained_labels.txt \
+  --image_dir=./FINAL_DATASETS/pipelineB
